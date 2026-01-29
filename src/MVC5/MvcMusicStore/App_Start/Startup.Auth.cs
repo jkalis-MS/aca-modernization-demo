70b975c8bdf061a014d72aab759a78fd5b3def46
Jan Kalis <jkalis@microsoft.com>
2026-01-28 16:21:36 -0800

    Replace ASP.NET Identity authentication with Microsoft Entra ID in Startup.Auth.cs

--- src/MVC5/MvcMusicStore/App_Start/Startup.Auth.cs
+++ src/MVC5/MvcMusicStore/App_Start/Startup.Auth.cs
@@ -1,38 +1,19 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.OWIN;
 using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 namespace MvcMusicStore
 {
     public partial class Startup
     {
-        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        // For more information on configuring authentication, please visit https://aka.ms/ms-id-web
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Enable the application to use a cookie to store information for the signed in user
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
-            });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            OwinTokenAcquirerFactory factory = TokenAcquirerFactory.GetDefaultInstance<OwinTokenAcquirerFactory>();
+            app.AddMicrosoftIdentityWebApp(factory);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
-
-            //app.UseGoogleAuthentication();
+            factory.Build();
         }
     }
 }
