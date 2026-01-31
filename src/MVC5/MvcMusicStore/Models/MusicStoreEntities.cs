49764a5e4de555186a9758c1c1e098388cf3f7c8
Jan Kalis <jkalis@microsoft.com>
2026-01-30 16:50:32 -0800

    * 'Line 1: Add the necessary using directive for Entity Framework's DbContext, which is required for MusicStoreEntities. Line 4: Restore the MusicStoreEntities class as a DbContext with DbSet properties for the application's models, as required by Entity Framework.' in file 'src\MVC5\MvcMusicStore\Models\MusicStoreEntities.cs'

--- src/MVC5/MvcMusicStore/Models/MusicStoreEntities.cs
+++ src/MVC5/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -1,17 +1,14 @@
-﻿
+﻿using System.Data.Entity;
+
 namespace MvcMusicStore.Models
 {
-    // Entity Framework is not referenced. To restore SaveChanges and DbSet functionality, inherit from DbContext and add the EntityFramework package.
-    // Example:
-    // public class MusicStoreEntities : System.Data.Entity.DbContext
-    // {
-    //     public System.Data.Entity.DbSet<Album> Albums { get; set; }
-    //     public System.Data.Entity.DbSet<Genre> Genres { get; set; }
-    //     public System.Data.Entity.DbSet<Artist> Artists { get; set; }
-    //     public System.Data.Entity.DbSet<Cart> Carts { get; set; }
-    //     public System.Data.Entity.DbSet<Order> Orders { get; set; }
-    //     public System.Data.Entity.DbSet<OrderDetail> OrderDetails { get; set; }
-    // }
-    //
-    // You must add the EntityFramework NuGet package to your project for this to work.
+public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
 }
